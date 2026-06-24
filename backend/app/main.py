72994b15ba30f0b4aad5f2d96e81d3beb065cb7f
Tomasz Pałus <palustomasz2@gmail.com>
2026-06-24 12:51:52 +0200

    Add Docker Compose with Postgres and Redis

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="MojFakturnik API")
+from app.config import settings
+
+app = FastAPI(
+    title=settings.APP_NAME
+)
 
 @app.get("/")
 def root():
     return {
         "status": "ok",
-        "service": "MojFakturnik"
+        "service": settings.APP_NAME
     }
